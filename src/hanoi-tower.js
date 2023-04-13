56689069bf32472cc04dfd8cbecf3b5d4408351c
Denis Ivanchenko <divanchenko@lt.life>
2023-04-13 11:25:14 +0300

    feat: add partial solution for hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi( disksNumber, turnsSpeed ) {
+  return {
+    turns: Math.pow(2, disksNumber) - 1,
+    seconds: Math.floor((Math.pow(2, disksNumber) - 1) / (turnsSpeed / 3600))
+  };
 }
 
 module.exports = {
