b93b7f45b2f6243153c4c46cd53d44ded11c9a74
Denis Ivanchenko <divanchenko@lt.life>
2023-04-14 13:36:06 +0300

    feat: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+  const halfLife = 5730;
+  const modernActivity = 15;
+
+  if (typeof sampleActivity !== 'string' || sampleActivity.length === 0) {
+    return false;
+  }
+  return 0;
 }
 
 module.exports = {
